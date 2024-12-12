911859b6edb8b230546051aa6b3e08547b30dc81
Mohit Negi <mnegi1433@gmail.com>
2024-12-12 20:15:58 +0530

    improved skills section

--- src/components/skills/Skill.tsx
+++ src/components/skills/Skill.tsx
@@ -6,15 +6,15 @@ import { SiPrisma, SiShadcnui } from "react-icons/si";
 import { FaGolang } from "react-icons/fa6";
 const Skill = () => {
     const skills = [
-        <IoLogoReact className="w-full h-full" />,
-        <RiNextjsFill className="w-full h-full" />,
-        <RiTailwindCssFill className="w-full h-full" />,
-        <DiNodejs className="w-full h-full" />,
-        <DiMongodb className="w-full h-full" />,
-        <BiLogoPostgresql className="w-full h-full" />,
-        <SiPrisma className="w-full h-full" />,
-        <SiShadcnui className="w-full h-full" />,
-        <FaGolang className="w-full h-full" />
+        <IoLogoReact className="w-full h-full relative z-40 group-hover:text-cyan-400"/>,
+        <RiNextjsFill className="w-full h-full relative z-40"/>,
+        <RiTailwindCssFill className="w-full h-full relative z-40 group-hover:text-cyan-500" />,
+        <DiNodejs className="w-full h-full relative z-40 group-hover:text-green-400" />,
+        <DiMongodb className="w-full h-full relative z-40 group-hover:text-green-400" />,
+        <BiLogoPostgresql className="w-full h-full relative z-40 group-hover:text-sky-500" />,
+        <SiPrisma className="w-full h-full relative z-40" />,
+        <SiShadcnui className="w-full h-full relative z-40" />,
+        <FaGolang className="w-full h-full relative z-40 group-hover:text-sky-500" />
     ]
 
     return (
@@ -23,7 +23,8 @@ const Skill = () => {
             <div className="flex flex-wrap gap-6 justify-center mt-4">
                 {skills.map((skill, index) => {
                     return (
-                        <div className="p-4 rounded-xl bg-[rgb(13,13,13)] hover:bg-secondary min-h-20 min-w-20 aspect-square" >
+                        <div className="group p-4 rounded-xl bg-[rgb(13,13,13)] hover:bg-secondary min-h-20 min-w-20 aspect-square relative" >
+                            <div className="w-0 bg-[rgb(13,13,13)] invisible group-hover:visible group-hover:w-full group-hover:bg-black h-full shadow-inner shadow-black transition-all duration-200 absolute top-0 left-0 rounded-xl z-30"></div>
                             {skill}
                         </div>
                     )
